881353421b0c42ddc36ed7e863d908c5fa78c9a7
Langston Graham <langstongraham@Langstons-MacBook-Pro.local>
2024-12-03 00:02:24 -0600

    Setup player at spawn, added movement

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,9 +1,13 @@
 extends Node2D
 
+@onready var player_spawn = $PlayerSpawnPosition
+var player = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	player = get_tree().get_first_node_in_group("player")
+	assert(player != null)
+	player.global_position = player_spawn.global_position
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
