9a0dc998021a768d8f7c16b66ba3985013721b72
Langston Graham <langstongraham@gmail.com>
2024-12-07 21:21:20 -0600

    Continuous fire

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,14 +1,21 @@
 extends CharacterBody2D
 
 @export var speed = 450
+@export var rate_of_fire := .25
 
 signal laser_shot(laser_scene, location)
 
 var laser_scene = preload("res://scenes/laser.tscn")
 
+var shoot_cooldown = false
+
 func _process(_delta):
-	if Input.is_action_just_pressed("shoot"):
-		shoot()
+	if Input.is_action_pressed("shoot"):
+		if !shoot_cooldown:
+			shoot_cooldown = true
+			shoot()
+			await get_tree().create_timer(rate_of_fire).timeout
+			shoot_cooldown = false
 
 func _physics_process(_delta: float) -> void:
 	var direction = Vector2(Input.get_axis("move_left", "move_right"), 
